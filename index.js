07146be49fb741a42784a19c8b79bab9a561b81b
ishaanchitre12 <81925256+ishaanchitre12@users.noreply.github.com>
2024-07-08 16:15:43 -0400

    Add API functionality to fetch book covers

--- index.js
+++ index.js
@@ -18,6 +18,32 @@ app.get("/new", (req, res) => {
     res.render("new.ejs");
 });
 
+app.post("/add", async (req, res) => {
+    try {
+        const googleResponse = await axios.get(GOOGLE_API_URL + "?q=" + encodeURI(req.body.newBook));
+        const data = googleResponse.data.items;
+        let books = [];
+        for (let i = 0; i < 5; i++) {
+            const book = data[i];
+            if (book.volumeInfo.imageLinks) {
+                books.push({
+                    id: book.id,
+                    imageLink: book.volumeInfo.imageLinks.thumbnail
+                });
+            }
+        }
+        res.render("new.ejs", {bookInfos: books});
+    } catch (err) {
+        console.error("Failed to make request:", err.message);
+        res.status(500).send("Failed to fetch activity");
+    }
+});
+
+app.post("/select", (req, res) => {
+    console.log(req.body.selectedBookId);
+    res.render("index.ejs");
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
