65611fdc340fb5303121d5aeb384f6654d318636
ishaanchitre12 <81925256+ishaanchitre12@users.noreply.github.com>
2024-07-07 20:49:30 -0400

    Create forms and install axios

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 import pg from "pg";
 
 const app = express();
 const port = 3000;
+const GOOGLE_API_URL = "https://www.googleapis.com/books/v1/volumes";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
-})
+});
+
+app.get("/new", (req, res) => {
+    res.render("new.ejs");
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
