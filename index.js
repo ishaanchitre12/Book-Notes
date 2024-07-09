8b9df88a2031aa8f6c1569935eaf8a952438f90d
ishaanchitre12 <81925256+ishaanchitre12@users.noreply.github.com>
2024-07-09 13:22:35 -0400

    Save book data in database and display it on homepage

--- index.js
+++ index.js
@@ -7,42 +7,85 @@ const app = express();
 const port = 3000;
 const GOOGLE_API_URL = "https://www.googleapis.com/books/v1/volumes";
 
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "books",
+    password: "B1llyButcher",
+    port: 5432,
+});
+  
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let books = [];
+const existingBooks = await db.query("select * from booksdata");
+existingBooks.rows.forEach(book => {
+    books.push({
+        id: book.id,
+        title: book.title,
+        author: book.author,
+        description: book.description,
+        imageLink: book.imagelink,
+        notes: book.notes
+    });
+});
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {bookData: books});
 });
 
 app.get("/new", (req, res) => {
     res.render("new.ejs");
 });
 
+let fivePossibleBooks = [];
 app.post("/add", async (req, res) => {
     try {
         const googleResponse = await axios.get(GOOGLE_API_URL + "?q=" + encodeURI(req.body.newBook));
         const data = googleResponse.data.items;
-        let books = [];
+        fivePossibleBooks = [];
         for (let i = 0; i < 5; i++) {
             const book = data[i];
             if (book.volumeInfo.imageLinks) {
-                books.push({
+                fivePossibleBooks.push({
                     id: book.id,
+                    title: book.volumeInfo.title,
+                    author: book.volumeInfo.authors[0],
+                    description: book.volumeInfo.description,
                     imageLink: book.volumeInfo.imageLinks.thumbnail
                 });
             }
         }
-        res.render("new.ejs", {bookInfos: books});
+        res.render("new.ejs", {bookInfos: fivePossibleBooks});
     } catch (err) {
         console.error("Failed to make request:", err.message);
         res.status(500).send("Failed to fetch activity");
     }
 });
 
-app.post("/select", (req, res) => {
-    console.log(req.body.selectedBookId);
-    res.render("index.ejs");
-})
+app.post("/select", async (req, res) => {
+    const selectedBook = fivePossibleBooks.find(book => book.id === req.body.selectedBookId);
+    const title = selectedBook.title;
+    const author = selectedBook.author;
+    const description = selectedBook.description;
+    const imageLink = selectedBook.imageLink;
+    const result = await db.query("insert into booksdata (title, author, description, imageLink)\
+        values ($1, $2, $3, $4)\
+        returning id", [title, author, description, imageLink]);
+    
+    books.push({
+        id: result.rows.id,
+        title: title,
+        author: author,
+        description: description,
+        imageLink: imageLink,
+        notes: null
+    });
+
+    res.redirect("/");
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
