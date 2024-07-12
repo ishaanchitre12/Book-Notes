3cafbe47933b5696ac87b30dad5d43d7d2fa90cb
ishaanchitre12 <81925256+ishaanchitre12@users.noreply.github.com>
2024-07-12 15:30:02 -0400

    Create new table in database to store notes. Add functionality to create a new note

--- index.js
+++ index.js
@@ -28,19 +28,57 @@ existingBooks.rows.forEach(book => {
         title: book.title,
         author: book.author,
         description: book.description,
-        imageLink: book.imagelink,
-        notes: book.notes
+        imageLink: book.imagelink
     });
 });
 app.get("/", (req, res) => {
     res.render("index.ejs", {bookData: books});
 });
 
+let fivePossibleBooks = [];
 app.get("/new", (req, res) => {
-    res.render("new.ejs");
+    res.render("new.ejs", {bookInfos: fivePossibleBooks});
 });
 
-let fivePossibleBooks = [];
+let notes = [];
+const existingNotes = await db.query("select * from notesdata");
+existingNotes.rows.forEach(note => {
+    notes.push({
+        id: note.id,
+        dateRead: note.date_read,
+        rating: note.rating,
+        notes: note.notes,
+        bookId: note.book_id
+    });
+});
+app.post("/notes", (req, res) => {
+    const selectedBook = books.find(b => b.id == req.body.selectedBookId);
+    const selectedNotes = notes.find(n => n.bookId == req.body.selectedBookId);
+    if (selectedNotes) {
+        res.render("notes.ejs", {book: selectedBook, note: selectedNotes});
+    } else {
+        res.render("notes.ejs", {book: selectedBook});
+    }
+});
+
+app.post("/new-note", async (req, res) => {
+    const dateRead = req.body.dateRead;
+    const rating = parseInt(req.body.rating);
+    const newNotes = req.body.notes;
+    const bookId = req.body.id;
+    const result = await db.query("insert into notesdata (date_read, rating, notes, book_id)\
+        values ($1, $2, $3, $4)\
+        returning id", [dateRead, rating, newNotes, bookId]);
+    notes.push({
+        id: result.rows.id,
+        dateRead: dateRead,
+        rating: rating,
+        notes: notes,
+        bookId: bookId
+    });
+    res.redirect("/");
+});
+
 app.post("/add", async (req, res) => {
     try {
         const googleResponse = await axios.get(GOOGLE_API_URL + "?q=" + encodeURI(req.body.newBook));
@@ -58,7 +96,7 @@ app.post("/add", async (req, res) => {
                 });
             }
         }
-        res.render("new.ejs", {bookInfos: fivePossibleBooks});
+        res.redirect("/new");
     } catch (err) {
         console.error("Failed to make request:", err.message);
         res.status(500).send("Failed to fetch activity");
@@ -80,8 +118,7 @@ app.post("/select", async (req, res) => {
         title: title,
         author: author,
         description: description,
-        imageLink: imageLink,
-        notes: null
+        imageLink: imageLink
     });
 
     res.redirect("/");
