e2b4590e8ddb9c1c0a1254c696d8d761aac89818
ishaanchitre12 <81925256+ishaanchitre12@users.noreply.github.com>
2024-07-14 17:26:42 -0400

    Modify frontend for edit notes page

--- index.js
+++ index.js
@@ -31,8 +31,28 @@ existingBooks.rows.forEach(book => {
         imageLink: book.imagelink
     });
 });
+let notes = [];
+const existingNotes = await db.query("select * from notesdata");
+existingNotes.rows.forEach(note => {
+    notes.push({
+        id: note.id,
+        noteDate: note.note_date,
+        notes: note.notes,
+        bookId: note.book_id
+    });
+});
+let reviews = [];
+const existingReviews = await db.query("select * from reviews");
+existingReviews.rows.forEach(review => {
+    reviews.push({
+        id: review.id,
+        dateFinished: review.date_finished,
+        rating: review.rating,
+        bookId: review.book_id
+    });
+});
 app.get("/", (req, res) => {
-    res.render("index.ejs", {bookData: books});
+    res.render("index.ejs", {bookData: books, notesData: notes});
 });
 
 let fivePossibleBooks = [];
@@ -40,42 +60,50 @@ app.get("/new", (req, res) => {
     res.render("new.ejs", {bookInfos: fivePossibleBooks});
 });
 
-let notes = [];
-const existingNotes = await db.query("select * from notesdata");
-existingNotes.rows.forEach(note => {
-    notes.push({
-        id: note.id,
-        dateRead: note.date_read,
-        rating: note.rating,
-        notes: note.notes,
-        bookId: note.book_id
-    });
-});
 app.post("/notes", (req, res) => {
     const selectedBook = books.find(b => b.id == req.body.selectedBookId);
-    const selectedNotes = notes.find(n => n.bookId == req.body.selectedBookId);
-    if (selectedNotes) {
-        res.render("notes.ejs", {book: selectedBook, note: selectedNotes});
-    } else {
-        res.render("notes.ejs", {book: selectedBook});
-    }
+    res.render("notes.ejs", {book: selectedBook, reviews: reviews});
 });
 
+app.post("/edit", async (req, res) => {
+    console.log(req.body.selectedBookId);
+    const selectedBookNotes = await db.query("select * from notesdata\
+        where book_id = $1", [req.body.selectedBookId]);
+    console.log(selectedBookNotes.rows);
+    res.render("edit.ejs", {notes: selectedBookNotes.rows, reviews: reviews});
+});
+
+app.post("/edit-review", async (req, res) => {
+    
+})
+
 app.post("/new-note", async (req, res) => {
-    const dateRead = req.body.dateRead;
-    const rating = parseInt(req.body.rating);
+    console.log(req.body);
+    const noteDate = req.body.noteDate;
     const newNotes = req.body.notes;
     const bookId = req.body.id;
-    const result = await db.query("insert into notesdata (date_read, rating, notes, book_id)\
-        values ($1, $2, $3, $4)\
-        returning id", [dateRead, rating, newNotes, bookId]);
+    const notesResult = await db.query("insert into notesdata (note_date, notes, book_id)\
+        values ($1, $2, $3)\
+        returning id", [noteDate, newNotes, bookId]);
     notes.push({
-        id: result.rows.id,
-        dateRead: dateRead,
-        rating: rating,
-        notes: notes,
+        id: notesResult.rows.id,
+        noteDate: noteDate,
+        notes: newNotes,
         bookId: bookId
     });
+    if (req.body.finished) {
+        const dateFinished = req.body.dateFinished;
+        const rating = parseInt(req.body.rating);
+        const reviewsResult = await db.query("insert into reviews (date_finished, rating, book_id)\
+            values ($1, $2, $3)\
+            returning id", [dateFinished, rating, bookId]);
+        reviews.push({
+            id: reviewsResult.rows.id,
+            dateFinished: dateFinished,
+            rating: rating,
+            bookId: bookId
+        });
+    }
     res.redirect("/");
 });
 
