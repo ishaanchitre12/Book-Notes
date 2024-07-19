3b06bd1110d7133e295c73c061d4d4e6a0007316
ishaanchitre12 <81925256+ishaanchitre12@users.noreply.github.com>
2024-07-18 20:42:50 -0400

    Complete edit front-end functionality

--- index.js
+++ index.js
@@ -68,14 +68,63 @@ app.get("/notes", (req, res) => {
     res.render("notes.ejs", {book: selectedBook, reviews: reviews});
 });
 
+let inProgressReviews = {};
+function checkInProgressReview(body) {
+    inProgressReviews = {
+        finished: body.finished ? "yes" : "no",
+        rating: body.rating,
+        dateFinished: body.dateFinished,
+        bookId: body.bookId
+    };
+}
+
+let inProgressNotes = [];
+function pushInProgressNotes(id, noteDate, notes, inProgress) {
+    const idIndex = id.findIndex(id => id == inProgress);
+    console.log(`id: ${id[idIndex]}, date: ${noteDate[idIndex]}, notes: ${notes[idIndex]}`);
+    // const inProgressIndex = inProgressNotes.findIndex(note => note.id == id[idIndex]);
+    // if (inProgressIndex) {
+    //     inProgressNotes[inProgressIndex] = {
+    //         id: id[idIndex],
+    //         noteDate: noteDate[idIndex],
+    //         notes: notes[idIndex]
+    //     };
+    // } else {
+    inProgressNotes.push({
+        id: id[idIndex],
+        noteDate: noteDate[idIndex],
+        notes: notes[idIndex]
+    });
+    // }
+}
+function checkInProgressNotes(body) {
+    if (Array.isArray(body.inProgressNotes)) {
+        body.inProgressNotes.forEach(p => {
+            pushInProgressNotes(body.id, body.noteDate, body.notes, p);
+        });
+    } else {
+        if (Array.isArray(body.id)) {
+            pushInProgressNotes(body.id, body.noteDate, body.notes, body.inProgressNotes);
+        } else {
+            inProgressNotes.push({
+                id: body.id,
+                noteDate: body.noteDate,
+                notes: body.notes
+            });
+        }
+    }
+}
+
 app.get("/edit", async (req, res) => {
     const selectedBookId = req.query.selectedBookId;
     const selectedBookNotes = await db.query("select * from notesdata\
         where book_id = $1", [selectedBookId]);
     const selectedBookImageLink = books.find(b => b.id == selectedBookId).imageLink;
-    console.log(selectedBookNotes.rows);
+    console.log(inProgressReviews);
     res.render("edit.ejs", {
         notes: selectedBookNotes.rows, 
+        inProgressReview: inProgressReviews,
+        inProgressNotes: inProgressNotes,
         reviews: reviews,
         cover: selectedBookImageLink
     });
@@ -83,7 +132,11 @@ app.get("/edit", async (req, res) => {
 
 app.post("/edit-review", async (req, res) => {
     console.log(req.body);
-    const index = reviews.findIndex(r => r.bookId == req.body.bookId);
+    inProgressReviews = {};
+    if (req.body.inProgressNotes) {
+        checkInProgressNotes(req.body);
+    }
+    const reviewsIndex = reviews.findIndex(r => r.bookId == req.body.bookId);
     if (req.body.finished) {
         const result = await db.query("update reviews\
             set rating = $1,\
@@ -91,30 +144,106 @@ app.post("/edit-review", async (req, res) => {
             where book_id = $3\
             returning id", [req.body.rating, req.body.dateFinished, req.body.bookId]);
 
-        reviews[index] = {
+        reviews[reviewsIndex] = {
             id: result.rows.id,
-            dateFinished: req.body.dateFinished,
+            dateFinished: new Date(req.body.dateFinished),
             rating: req.body.rating,
             bookId: req.body.bookId
         };
     } else {
         await db.query("delete from reviews where book_id = $1", [req.body.bookId]);
-        reviews.splice(index, 1);
+        reviews.splice(reviewsIndex, 1);
     }
     console.log(reviews);
     res.redirect(`/edit?selectedBookId=${req.body.bookId}`);
 });
 
 app.post("/edit-notes", async (req, res) => {
+    console.log(req.body);
+    const index = inProgressNotes.findIndex(note => note.id == req.body.submittedNote);
+    if (index >= 0) {
+        inProgressNotes.splice(index, 1);
+    }
+    if(req.body.inProgressReview || inProgressReviews.finished) {
+        checkInProgressReview(req.body);
+    }
+    if(req.body.inProgressNotes) {
+        checkInProgressNotes(req.body);
+    }
+    let id = "";
+    let noteDate = "";
+    let newNotes = "";
+    if (Array.isArray(req.body.id)) {
+        const referringIndex = req.body.id.findIndex(id => id == req.body.submittedNote);
+        id = req.body.id[referringIndex];
+        noteDate = req.body.noteDate[referringIndex];
+        newNotes = req.body.notes[referringIndex];
+    } else {
+        id = req.body.id;
+        noteDate = req.body.noteDate;
+        newNotes = req.body.notes;
+    }
     await db.query("update notesdata\
         set note_date = $1,\
         notes = $2\
-        where id = $3", [req.body.noteDate, req.body.notes, req.body.id]);
-    const index = notes.findIndex(n => n.id == req.body.id);
-    notes[index].id = parseInt(req.body.id);
-    notes[index].noteDate = req.body.noteDate;
-    notes[index].notes = req.body.notes;
-    res.redirect(`/edit?selectedBookId=${notes[index].bookId}`);
+        where id = $3", [noteDate, newNotes, id]);
+    const notesIndex = notes.findIndex(n => n.id == id);
+    notes[notesIndex].id = parseInt(id);
+    notes[notesIndex].noteDate = noteDate;
+    notes[notesIndex].notes = newNotes;
+    res.redirect(`/edit?selectedBookId=${notes[notesIndex].bookId}`);
+});
+
+app.post("/delete-review", async (req, res) => {
+    inProgressReviews = {};
+    const index = reviews.findIndex(review => review.bookId == req.body.bookId);
+    reviews.splice(index, 1);
+    await db.query("delete from reviews where book_id = $1", [req.body.bookId]);
+    const remainingNotes = notes.find(note => note.bookId == req.body.bookId);
+    const remainingReview = reviews.find(review => review.bookId == req.body.bookId);
+    if (remainingNotes || remainingReview) {
+        return res.redirect(`/edit?selectedBookId=${req.body.bookId}`);
+    } else {
+        return res.redirect("/");
+    }
+});
+
+app.post("/delete-note", async (req, res) => {
+    console.log(req.body);
+    const inProgressIndex = inProgressNotes.findIndex(note => note.id == req.body.deletedNote);
+    if (inProgressIndex >= 0) {
+        inProgressNotes.splice(inProgressIndex, 1);
+    }
+    const notesIndex = notes.findIndex(note => note.id == req.body.deletedNote);
+    notes.splice(notes, 1);
+    await db.query("delete from notesdata where id = $1", [req.body.deletedNote]);
+    const remainingNotes = notes.find(note => note.bookId == req.body.bookId);
+    const remainingReview = reviews.find(review => review.bookId == req.body.bookId);
+    if (remainingNotes || remainingReview) {
+        return res.redirect(`/edit?selectedBookId=${req.body.bookId}`);
+    } else {
+        return res.redirect("/");
+    }
+});
+
+app.post("/cancel-review-progress", async (req, res) => {
+    inProgressReviews = {};
+    if (req.body.inProgressNotes) {
+        checkInProgressNotes(req.body);
+    }
+    res.redirect(`/edit?selectedBookId=${req.body.bookId}`);
+});
+
+app.post("/cancel-note-progress", async (req, res) => {
+    console.log(req.body);
+    if (req.body.inProgressNotes) {
+        checkInProgressNotes(req.body);
+    } 
+    const index = inProgressNotes.findIndex(note => note.id == req.body.cancelledNote);
+    if (index >= 0) {
+        inProgressNotes.splice(index, 1);
+    }
+    res.redirect(`/edit?selectedBookId=${req.body.bookId}`);
 });
 
 app.post("/new-note", async (req, res) => {
@@ -139,7 +268,7 @@ app.post("/new-note", async (req, res) => {
             returning id", [dateFinished, rating, bookId]);
         reviews.push({
             id: reviewsResult.rows.id,
-            dateFinished: dateFinished,
+            dateFinished: new Date(dateFinished),
             rating: rating,
             bookId: bookId
         });
